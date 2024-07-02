4642aaec4120e9e69ae032ef3268c99a67f7966f
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-07-02 14:32:32 +0400

    Commit initial version of Mini App

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('JS code here')
+    // const app = document.getElementById('app');
+  
+    // // Example of calling a 3rd party API
+    // fetch('https://api.example.com/data')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     app.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+    //   })
+    //   .catch(error => {
+    //     console.error('Error fetching data:', error);
+    //     app.innerHTML = "<p>Error fetching data.</p>";
+    //   });
+});
