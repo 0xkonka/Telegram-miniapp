3227db7112dd718db9d023e528065912cdd0893d
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-07-09 02:13:00 +0400

    Test UserId and name passing without param

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -2,4 +2,4 @@ window.config = {
   // BE_URL: "http://localhost:8000/api/telegram", // test
   BE_URL: "https://be-express-lime.vercel.app/api/telegram",
   TG_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZWNyZXQiOiJ0cmVuIiwicm9sZSI6InRlbGVncmFtIiwiaWF0IjoxNzE5OTM2MTI5fQ.H6jyqpfVwb2aAOWTozuyUzX5zjJauegqUWWAY0y9RXY"
-};I 
+};
