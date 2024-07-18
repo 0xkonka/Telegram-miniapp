cbda8367d6b969da165b654503267a7413a338e0
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-07-18 13:20:51 -0400

    revert bot.py

--- public/scripts/farm.js
+++ public/scripts/farm.js
@@ -21,9 +21,9 @@ function getUserInfo() {
 }
 
 // Completed Farming to send notification
-async function completedFarming() {
+function completedFarming() {
   const data = {};
-  await fetch('https://telegram.tren.finance/completed-farming', {
+  fetch('https://telegram.tren.finance/completed-farming', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
@@ -54,6 +54,14 @@ document.addEventListener("DOMContentLoaded", async () => {
   Telegram.WebApp.expand();
   Telegram.WebApp.ready();
 
+  // Telegram.WebApp.onEvent("viewportChanged", function (height) {
+  //   if (height == window.innerHeight) {
+  //     return;
+  //   }
+  //   Telegram.WebApp.expand();
+  // });
+  // Immediately attempt to expand
+  // Telegram.WebApp.expand();
   checkHeight()
 
   // ==================================== //
@@ -104,10 +112,6 @@ document.addEventListener("DOMContentLoaded", async () => {
           document.getElementById("remaining-time").textContent =
             "0h : 0m : 0s";
         }
-
-        // Display tasks badge content
-        const completedTasks = ["telegram", "discord", "twitter"].filter(social => userStatus.socialTaskStatus[social]).length;
-        document.getElementById("tasks-badge").innerHTML = 3 - completedTasks;
 
         // Start interval to update points and time every second
         setInterval(() => {
@@ -170,15 +174,10 @@ document.addEventListener("DOMContentLoaded", async () => {
       document.getElementById(
         "remaining-time"
       ).textContent = `${hours}h : ${minutes}m : ${seconds}s`;
-      // document.getElementById(
-      //   "remaining-seconds"
-      // ).textContent = remainingTime;
       disableFarmingButton();
-    }
-    // else if (remainingTime == 0) {
-    //   completedFarming();
-    // }
-    else {
+    } else if(remainingTime == 0) {
+      completedFarming();
+    } else {
       document.getElementById("remaining-time").textContent = "0h : 0m : 0s";
       enableFarmingButton();
     }
