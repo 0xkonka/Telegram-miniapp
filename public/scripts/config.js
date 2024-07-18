f1923a288c26e6432ccf7eb620a2b74ed0cb84b0
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-07-18 15:50:53 -0400

    update backend url

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,4 +1,4 @@
 window.config = {
   // BE_URL: "http://localhost:8000/api/telegram", // test
-  BE_URL: "https://be-express-lime.vercel.app/api/telegram",
+  BE_URL: "https://api.tren.finance/api/telegram",
 };
